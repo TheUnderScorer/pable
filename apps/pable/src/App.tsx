5f092e81812c8ba2885939aae63b54a0e53b8c0b
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-03-22 15:41:37 +0100

    feat(pable): Show version

--- apps/pable/src/App.tsx
+++ apps/pable/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Button, Container } from '@chakra-ui/react';
+import { Box, Container, Text } from '@chakra-ui/react';
 import { TranslationsTableView } from '@pable/domain-frontend';
 
 export const App = () => {
   return (
     <Container p={4} maxW="1100px">
       <TranslationsTableView />
+      <Box position="fixed" bottom="0" left="0">
+        <Text fontSize="xs">
+          Version: {process.env.NX_APP_VERSION ?? 'Local'}
+        </Text>
+      </Box>
     </Container>
   );
 };
