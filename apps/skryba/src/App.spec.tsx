4c9c1e4e91c50a460e1a9d6cfeff16a458bf9028
Przemysław Żydek <przemyslawzydek@gmail.com>
2021-03-31 08:24:23 +0200

    test: Fix test

--- apps/skryba/src/App.spec.tsx
+++ apps/skryba/src/App.spec.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ApiClientProvider } from '@skryba/shared-frontend';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('App', () => {
   it('should render successfully', async () => {
     const cmp = render(
       <QueryClientProvider client={new QueryClient()}>
-        <ApiClientProvider url="">
-          <App />
-        </ApiClientProvider>
+        <MemoryRouter>
+          <ApiClientProvider url="">
+            <App />
+          </ApiClientProvider>
+        </MemoryRouter>
       </QueryClientProvider>
     );
 
